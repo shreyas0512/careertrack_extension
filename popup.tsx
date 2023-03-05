43d58a9e765adc5ec937912d99131c0c8335e058
shreyas0512 <shreyassanthosh362@gmail.com>
2023-03-05 05:56:33 +0530

    career_track_extension

--- popup.tsx
+++ popup.tsx
@@ -1,26 +1,72 @@
 import { useState } from "react"
 
-function IndexPopup() {
-  const [data, setData] = useState("")
+import "./popup.css"
 
+function IndexPopup() {
+  const [role, setRole] = useState("")
+  const [company, setCompany] = useState("")
+  const [description, setDescription] = useState("")
+  const [location, setLocation] = useState("")
+  const [selectedValue, setSelectedValue] = useState()
+  const handleDrop = (e: any) => {
+    setSelectedValue(e.target.value)
+  }
+  function onSub() {
+    fetch("https://foss-hack-api1.vercel.app/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        // Data to be sent in the request body
+        company: company,
+        jobtitle: role,
+        joblocation: location,
+        status: selectedValue
+      })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data
+        console.log(data)
+      })
+      .catch((error) => {
+        // Handle any errors that occur
+        console.error(error)
+      })
+  }
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="bg">
+      <h3>Enter Job Role: </h3>
+      <div className="textouter">
+        <input type="text" onChange={(e) => setRole(e.target.value)} />
+      </div>
+      <h3>Enter Company Name: </h3>
+      <div className="textouter">
+        <input type="text" onChange={(e) => setCompany(e.target.value)} />
+      </div>
+      <h3>Location: </h3>
+      <div className="textouter">
+        <input type="text" onChange={(e) => setLocation(e.target.value)} />
+      </div>
+      <h3>Application Type: </h3>
+      <div className="textouter">
+        <select
+          className="applntype"
+          style={{}}
+          value={selectedValue}
+          onChange={handleDrop}>
+          <option selected value="Applied">
+            Applied
+          </option>
+          <option value="Interview">Interview</option>
+          <option value="Wishlist">Wishlist</option>
+          <option value="Offers">Offers</option>
+        </select>
+      </div>
+      <button className="btn" onClick={onSub}>
+        ADD
+      </button>
     </div>
   )
 }
